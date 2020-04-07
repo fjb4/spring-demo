a64729b52d725b58d5c0e1124fb09369d306d573
John Bush <fbush@pivotal.io>
2020-04-07 15:09:10 -0500

    initial commit

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long personId;
+	
+	@Column(nullable=false)
+	private String firstName;
+
+    @Column(nullable=false)
+    private String lastName;
+    
+    public Long getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Long personId) {
+        this.personId = personId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
